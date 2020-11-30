b2514346446ee37a334dfdacfc81fda2dcc650ab
ste2021 <estela.oliveira.cont@outlook.com>
2020-11-30 10:44:50 -0300

    ts

--- TYPESCRIPT/TYPESCRIPT1/alurabank/app/js/views/NegociacoesView.js
+++ TYPESCRIPT/TYPESCRIPT1/alurabank/app/js/views/NegociacoesView.js
@@ -1,22 +1,36 @@
 class NegociacoesView {
-    template() {
+    constructor(seletor) {
+        this._elemento = document.querySelector(seletor);
+    }
+    update(model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+    template(model) {
         return `
-          <table class=" table table-hover table-bordered">
-            <thead>
+      <table class="table table-hover table-bordered">
+          <thead>
               <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
+                  <th>DATA</th>
+                  <th>QUANTIDADE</th>
+                  <th>VALOR</th>
+                  <th>VOLUME</th>
               </tr>
-            </thead>
-            
-            <tbody>
-            </tbody>
+          </thead>
 
-            <tfoot>
-            </tfoot>
-        </table>
-        `;
+          <tbody>
+              ${model.paraArray().map(negociacao => `
+                      <tr>
+                          <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
+                          <td>${negociacao.quantidade}</td>
+                          <td>${negociacao.valor}</td>
+                          <td>${negociacao.volume}</td>
+                      <tr>
+                   `).join('')}            
+          </tbody>
+
+          <tfoot>
+          </tfoot>
+      </table> 
+      `;
     }
 }
