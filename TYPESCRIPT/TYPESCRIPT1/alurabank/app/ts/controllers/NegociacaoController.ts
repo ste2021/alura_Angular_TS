3e328645926c5115adfe2c295e7f8b22138e35e9
ste2021 <estela.oliveira.cont@outlook.com>
2020-12-02 12:10:27 -0300

    initialTS2

--- TYPESCRIPT/TYPESCRIPT1/alurabank/app/ts/controllers/NegociacaoController.ts
+++ TYPESCRIPT/TYPESCRIPT1/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,12 +1,16 @@
-class NegociacaoController {
+import { NegociacoesView } from '../views/NegociacoesView';
+import { MensagemView } from '../views/MensagemView';
+import { Negociacoes } from '../models/Negociacoes';
+import { Negociacao } from '../models/Negociacao';
+
+export class NegociacaoController {
 
     private _inputData: JQuery;
     private _inputQuantidade: JQuery;
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
-    private _negociacoesView = new Views.NegociacoesView('#negociacoesView');
-    private _mensagemView = new Views.MensagemView('#mensagemView');
-
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
         this._inputData = $('#data');
@@ -26,7 +30,8 @@ class NegociacaoController {
         );
 
         this._negociacoes.adiciona(negociacao);
+
         this._negociacoesView.update(this._negociacoes);
-        this._mensagemView.update('Negociação adicionada com sucesso');
+        this._mensagemView.update('Negociação adicionada com sucesso!');
     }
 }
--- TYPESCRIPT/TYPESCRIPT1/alurabank/app/ts/models/Negociacao.ts
+++ TYPESCRIPT/TYPESCRIPT1/alurabank/app/ts/models/Negociacao.ts
@@ -1,18 +1,24 @@
-class Negociacao{
+export class Negociacao {
 
-    constructor(private _data: Date, private _quantidade: number, private _valor:number) {}
-    get data(){
+    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
+
+    get data() {
+
         return this._data;
     }
-    
-    get quantidade(){
+
+    get quantidade() {
+
         return this._quantidade;
     }
 
-    get valor(){
+    get valor() {
+
         return this._valor;
     }
-    get volume(){
+
+    get volume() {
+
         return this._quantidade * this._valor;
     }
 }
