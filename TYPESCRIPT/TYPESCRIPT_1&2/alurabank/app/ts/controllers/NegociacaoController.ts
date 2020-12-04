e1e57ae22ba8ecf9bf1397dfce48469184b4175b
ste2021 <estela.oliveira.cont@outlook.com>
2020-12-04 16:06:30 -0300

    terminandoTS2

--- TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/ts/controllers/NegociacaoController.ts
+++ TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,7 +1,5 @@
-import { NegociacoesView } from '../views/NegociacoesView';
-import { MensagemView } from '../views/MensagemView';
-import { Negociacoes } from '../models/Negociacoes';
-import { Negociacao } from '../models/Negociacao';
+import { NegociacoesView, MensagemView } from '../views/index';
+import { Negociacao, Negociacoes } from '../models/index';
 
 export class NegociacaoController {
 
@@ -11,7 +9,7 @@ export class NegociacaoController {
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
-
+    
     constructor() {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
@@ -23,8 +21,16 @@ export class NegociacaoController {
 
         event.preventDefault();
 
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)) {
+
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return 
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')), 
+            data, 
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         );
@@ -34,4 +40,20 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação adicionada com sucesso!');
     }
+
+    private _ehDiaUtil(data: Date) {
+
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+}
+//pra impedir que as negociacoes sejam feitos em dias não úteis
+enum DiaDaSemana {
+
+    Domingo, 
+    Segunda, 
+    Terca, 
+    Quarta, 
+    Quinta, 
+    Sexta, 
+    Sabado
 }
--- TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/ts/views/MensagemView.ts
+++ TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/ts/views/MensagemView.ts
@@ -8,3 +8,4 @@ export class MensagemView extends View<string> {
     }
 
 }
+
