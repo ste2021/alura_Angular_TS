6435122a775ffefb9594f049ae878add98fb7dbb
ste2021 <estela.oliveira.cont@outlook.com>
2020-12-05 17:07:07 -0300

    decoratorsTS

--- TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/js/helpers/decorators/logarTempoDeExecucao.js
+++ TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/js/helpers/decorators/logarTempoDeExecucao.js
@@ -3,6 +3,12 @@ System.register([], function (exports_1, context_1) {
     var __moduleName = context_1 && context_1.id;
     function logarTempoDeExecucao() {
         return function (target, propertyKey, descriptor) {
+            const metodoOriginal = descriptor.value;
+            descriptor.value = function (...args) {
+                const retorno = metodoOriginal.apply(this, args);
+                return retorno;
+            };
+            return descriptor;
         };
     }
     exports_1("logarTempoDeExecucao", logarTempoDeExecucao);
--- TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/ts/helpers/decorators/dist/logarTempoDeExecucao.js
+++ TYPESCRIPT/TYPESCRIPT_1&2/alurabank/app/ts/helpers/decorators/dist/logarTempoDeExecucao.js
@@ -3,6 +3,16 @@ exports.__esModule = true;
 exports.logarTempoDeExecucao = void 0;
 function logarTempoDeExecucao() {
     return function (target, propertyKey, descriptor) {
+        var metodoOriginal = descriptor.value;
+        descriptor.value = function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            var retorno = metodoOriginal.apply(this, args);
+            return retorno;
+        };
+        return descriptor;
     };
 }
 exports.logarTempoDeExecucao = logarTempoDeExecucao;
